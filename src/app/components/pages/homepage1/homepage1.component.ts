82397c3907a5e04c1bb17ddc919147c84d5125df
smn <jr.tap@hotmail.com>
2021-12-02 23:36:26 -0700

    Inserción de texto

--- src/app/components/pages/homepage1/homepage1.component.ts
+++ src/app/components/pages/homepage1/homepage1.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-homepage1',
-  templateUrl: './homepage1.component.html',
-  styleUrls: ['./homepage1.component.css']
+    selector: 'app-homepage1',
+    templateUrl: './homepage1.component.html',
+    styleUrls: ['./homepage1.component.css']
 })
 export class Homepage1Component implements OnInit {
+    titulos: Observable<any[]>;
+    
+    constructor(firestore: AngularFirestore) {
+	this.titulos = firestore.collection('titulos').valueChanges();
+    }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
